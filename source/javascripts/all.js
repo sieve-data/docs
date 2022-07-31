73fcd12b591afb606d753a85cdbfa1315db23438
Mokshith Voodarla <mvoodarla@gmail.com>
2022-07-31 11:05:10 -0700

    remove

--- source/javascripts/all.js
+++
@@ -1,9 +0,0 @@
-//= require ./all_nosearch
-//= require ./app/_search
-window.intercomSettings = {
-  api_base: "https://api-iam.intercom.io",
-  app_id: "whro4538"
-};
-
-// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/whro4538'
-(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/whro4538';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
